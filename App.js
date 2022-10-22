8881db2491b399b4c963c475454c8f8f0e42a716
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2022-10-21 19:11:01 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
+import UpdateScreen from "./screens/Updates";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
